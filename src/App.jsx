dc74a9e957e8cd18e543306e086b5d6238c371f2
narkaraditya10 <53488784+narkaraditya10@users.noreply.github.com>
2024-06-18 22:25:22 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 
 import "./App.css";
+import ButtonGradient from "../src/assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
 function App() {
-  return <>
-  </>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login">
+          somthe
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
 }
 
 export default App;
